4f2286151756e2eac08f5c92de53ea47bdce0ae5
Deracodes1 <ezemeniachidera@gmail.com>
2025-12-09 01:08:09 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const allProducts = document.querySelectorAll(".grid-item");
+const productAlert = document.querySelector(".product-alert");
+const alertMsg = document.querySelector(".alert-message");
+const overlay = document.querySelector(".overlay");
+allProducts.forEach((product) => {
+  const productNameEl = product.querySelector(".product-name");
+  product.addEventListener("click", function () {
+    // alert(productNameEl.textContent);
+
+    // improved alert with modal window for better ux
+    productAlert.style.visibility = "visible";
+    overlay.style.display = "block";
+    alertMsg.textContent = `The current highlighted product is ${productNameEl.textContent}.`;
+  });
+});
+
+overlay.addEventListener("click", function () {
+  productAlert.style.visibility = "hidden";
+  overlay.style.display = "none";
+});
